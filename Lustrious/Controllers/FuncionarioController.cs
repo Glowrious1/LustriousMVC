b6e81a620706057e3e1bfdb3919ce476511fed6f
Gabriel <gabrielcorolleite@gmail.com>
2025-09-26 14:55:37 -0300

    Iniciando na master

---
+++ Lustrious/Controllers/FuncionarioController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lustrious.Controllers
+{
+    public class FuncionarioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
