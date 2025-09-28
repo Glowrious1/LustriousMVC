1f2847b8968ea1723333cb5c31e33f9fb9cfa8d6
Dangar-Dev <danielsobraldev@gmail.com>
2025-09-28 13:04:02 -0300

    criando o metodo index para listar os clientes

--- Lustrious/Controllers/ClienteController.cs
+++ Lustrious/Controllers/ClienteController.cs
@@ -1,12 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Lustrious.Data;
+using Lustrious.Models;
+using MySql.Data.MySqlClient;
 
 namespace Lustrious.Controllers
 {
     public class ClienteController : Controller
     {
+        public readonly DataBase db = new DataBase();
         public IActionResult Index()
+
         {
-            return View();
+            var lista = new List<Cliente>();
+            using var conn = db.GetConnection();
+            using var cmd = new MySqlCommand("selectCliente", conn) { CommandType = System.Data.CommandType.StoredProcedure };
+            using var rd = cmd.ExecuteReader();
+            while (rd.Read())
+            {
+                lista.Add(new Cliente
+                {
+                    IdClient = rd.GetInt32("IdClient"),
+                    Nome = rd.GetString("nome"),
+                    Email = rd.GetString("email"),
+                    CPF = rd.GetInt32("CPF"),
+                    Senha = rd.GetString("Senha")
+                });
+            }
+            return View(lista);
         }
     }
 }
