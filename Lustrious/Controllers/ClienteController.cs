1dde25df712cebc273270152219b285031697cb7
Dangar-Dev <danielsobraldev@gmail.com>
2025-09-28 15:18:50 -0300

    Criand os metodos criar,editar e excluir

--- Lustrious/Controllers/ClienteController.cs
+++ Lustrious/Controllers/ClienteController.cs
@@ -3,6 +3,7 @@ using Lustrious.Data;
 using Lustrious.Models;
 using MySql.Data.MySqlClient;
 using System.Data;
+using Microsoft.Net.Http.Headers;
 
 namespace Lustrious.Controllers
 {
@@ -28,6 +29,84 @@ namespace Lustrious.Controllers
                 });
             }
             return View(lista);
+        }
+        public IActionResult CriarCliente()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CriarCliente(Cliente cliente)
+        {
+            using var conn = db.GetConnection();
+            using var cmd = new MySqlCommand("InsertCliente", conn);
+            cmd.Parameters.AddWithValue("vNome", cliente.Nome);
+            cmd.Parameters.AddWithValue ("vEmail", cliente.Email);
+            cmd.Parameters.AddWithValue("vSenha", cliente.Senha);
+            cmd.Parameters.AddWithValue("vCPF", cliente.CPF);
+            cmd.ExecuteNonQuery();
+            return RedirectToAction("CriarAutor");
+        }
+        [HttpGet]
+        public IActionResult EditarCliente(int id)
+        {
+             using var conn = db.GetConnection();   
+             Cliente? cliente = null;
+            using (var cmd = new MySqlCommand("ObterCliente", conn) { CommandType = System.Data.CommandType.StoredProcedure })
+            {
+                cmd.Parameters.AddWithValue("vId", id);
+                using var rd = cmd.ExecuteReader();
+                if (rd.Read())
+                {
+                    cliente = new Cliente
+                    {
+                        IdClient = rd.GetInt32("vIdClient"),
+                        Nome = rd.GetString("vNome"),
+                        Email = rd.GetString("vEmail"),
+                        Senha = rd.GetString("vSenha"),
+                        CPF = rd.GetString("vCPF")
+
+                    };
+                }
+            }
+            return View(cliente);
+        }
+        [HttpPost, ValidateAntiForgeryToken]
+
+         public IActionResult EditarCliente(Cliente model)
+        {
+            if (model.IdClient <= 0) return NotFound();
+            if(string.IsNullOrWhiteSpace(model.Email))
+              {
+                ModelState.AddModelError("", "Informe seu Email");
+            }
+            using var conn2 = db.GetConnection();
+            using var cmd = new MySqlCommand("UpdateCliente", conn2) { CommandType = System.Data.CommandType.StoredProcedure };
+            cmd.Parameters.AddWithValue("vId",model.IdClient);
+            cmd.Parameters.AddWithValue("vNome",model.Nome);
+            cmd.Parameters.AddWithValue("vEmail", model.Email);
+            cmd.Parameters.AddWithValue("vSenha", model.Senha);
+            cmd.Parameters.AddWithValue("vCPF",model.CPF);
+            cmd.ExecuteNonQuery();
+
+            TempData["ok"] = "Cliente Atualizado!";
+            return RedirectToAction(nameof(Index));
+        }
+        [HttpPost, ValidateAntiForgeryToken]
+        public IActionResult Excluir(int id)
+        {
+            using var conn = db.GetConnection();
+            try
+            {
+                using var cmd = new MySqlCommand("DeleteCliente", conn) { CommandType = System.Data.CommandType.StoredProcedure };
+                cmd.Parameters.AddWithValue("vIdClient", id);
+                cmd.ExecuteNonQuery();
+                TempData["ok"] = "Autor Excluido!";
+            }
+            catch (MySqlException ex)
+            {
+                TempData["ok"]=ex.Message;
+            }
+            return RedirectToAction(nameof(Index));
         }
     }
 }
