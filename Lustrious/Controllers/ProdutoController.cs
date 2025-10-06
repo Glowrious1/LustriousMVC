4af512255f8681f6771dde00084450441883f7a0
Gabriel <gabrielcorolleite@gmail.com>
2025-10-06 14:36:39 -0300

    Criando index produto

--- Lustrious/Controllers/ProdutoController.cs
+++ Lustrious/Controllers/ProdutoController.cs
@@ -1,12 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Lustrious.Data;
+using Microsoft.AspNetCore.Mvc;
+using Lustrious.Models;
+using MySql.Data.MySqlClient;
+
 
 namespace Lustrious.Controllers
 {
     public class ProdutoController : Controller
     {
+        public readonly DataBase db = new DataBase();
+
         public IActionResult Index()
         {
-            return View();
+            var lista = new List<Produto>();
+            using var conn = db.GetConnection();
+            using var cmd = new MySqlCommand("selectProduto", conn) { CommandType = System.Data.CommandType.StoredProcedure}
+            using var rd = cmd .ExecuteReader();
+            while (rd.Read())
+            {
+                lista.Add(new Produto
+                {
+                    CodigoBarras = rd.GetInt32("CodigoBarras"),
+                    NomeProd = rd.GetString("NomeProd"),
+                    qtd = rd.GetInt32("qtd"),
+                    Descricao = rd.GetString("Descricao"),
+                    ValorUnitario = rd.GetDecimal("ValorUnitario")
+                });
+            }
+            
+            return View(lista);
         }
     }
 }
