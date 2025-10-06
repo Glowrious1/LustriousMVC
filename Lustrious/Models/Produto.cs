80b7662fa084b5480361beb2b65573e6a94680f6
Gabriel <gabrielcorolleite@gmail.com>
2025-10-06 14:14:27 -0300

    Criando model produto

--- Lustrious/Models/Produto.cs
+++ Lustrious/Models/Produto.cs
@@ -2,5 +2,12 @@
 {
     public class Produto
     {
+        public int CodigoBarras { get; set; }
+        public string NomeProd { get; set; }
+        public int qtd { get; set; }
+        public string Descricao { get; set; }
+        public decimal ValorUnitario { get; set; }
+
+
     }
 }
