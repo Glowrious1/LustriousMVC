5c5aff750cefce1b94303bc8af9582e3d3dd697d
HenriqueCO17 <ohenriquecunha@gmail.com>
2025-11-13 16:17:55 -0300

    Adicionando mais conteudo no FuncionarioRepositorio

--- Lustrious/Repositorio/FuncionarioRepositorio.cs
+++ Lustrious/Repositorio/FuncionarioRepositorio.cs
@@ -9,7 +9,7 @@ namespace Lustrious.Repositorio
 {
     public class FuncionarioRepositorio : IFuncionarioRepositorio
     {
-        private readonly DataBase dataBase;
+        private readonly DataBase _dataBase;
 
         public FuncionarioRepositorio(DataBase dataBase)
         {
@@ -27,12 +27,48 @@ namespace Lustrious.Repositorio
                     cmd.Parameters.AddWithValue("vEmail", funcionario.Email);
                     cmd.Parameters.AddWithValue("vCPF", funcionario.CPF);
                     cmd.Parameters.AddWithValue("vSenha", funcionario.Senha);
-                    cmd.Parameters.AddWithValue("vRole", "Funcionario");
+                    cmd.Parameters.AddWithValue("vRole", funcionario.Role);
                     cmd.Parameters.AddWithValue("vSexo", funcionario.Sexo);
                     cmd.Parameters.AddWithValue("vFoto", funcionario.Foto);
                     cmd.ExecuteNonQuery();
                 }
             }
         }
+        public Usuario AcharFuncionario(int id)
+        {
+            Usuario funcionario = new Usuario();
+            using (var conexao = _dataBase.GetConnection())
+            {
+                conexao.Open();
+                using (var cmd = new MySqlCommand("obterUsuario", conexao))
+                {
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("vId", id);
+
+                    MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+
+                    DataTable dt = new DataTable();
+
+                    da.Fill(dt);
+
+                    conexao.Close();
+
+                    foreach (DataRow dr in dt.Rows)
+                    {
+                        funcionario = new Usuario()
+                        {
+                            IdUser = Convert.ToInt32(dr["IdUser"]),
+                            Nome = (string)dr["Nome"],
+                            Email = (string)dr["Email"],
+                            Senha = (string)dr["Senha"],
+                            Sexo = (string)dr["Sexo"],
+                            CPF = (string)dr["CPF"]
+                        };
+                    }
+                }
+            }
+            return funcionario;
+        }
     }
 }
+
