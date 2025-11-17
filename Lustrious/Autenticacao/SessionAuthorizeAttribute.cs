7099988b4953428f3c87c9bb9f467989d980058c
Dangar-Dev <danielsobraldev@gmail.com>
2025-11-17 07:51:07 -0300

    Criando a authenticação no projeto , Sua controller + repositorio

---
+++ Lustrious/Autenticacao/SessionAuthorizeAttribute.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Lustrious.Autenticacao
+{
+    public class SessionAuthorizeAttribute : ActionFilterAttribute
+    {
+        public string? RoleAnyOf { get; set; }
+
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            var http = context.HttpContext;
+            var role  = http.Session.GetString(SessionsKeys.UserRole);
+            var userId = http.Session.GetString(SessionsKeys.UserId);
+
+            if (userId == null)
+            {
+                context.Result = new RedirectToActionResult("Login", "Autenticacao", null);
+                return;
+            }
+            if (!string.IsNullOrWhiteSpace(RoleAnyOf))
+            {
+                var allowedRoles = RoleAnyOf.Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+                if (!allowedRoles.Contains(role))
+                {                    
+                    context.Result = new RedirectToActionResult("AcessoNegado", "Auth", null);
+                    return;
+                }
+            }
+            base.OnActionExecuted(context);
+        }
+    }
+}
